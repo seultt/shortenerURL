03daa1ae669eeb75aaa007d4000ef466a32d2a54
seultt <seultt@gmail.com>
2017-08-31 14:00:43 +0900

    입력 form  생성 data에 입력값 담기

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const data = [
 ]
 
 const app = express()
-// 로그인 안하면 막는 기능
+// 로그인( 안하면 막는 )기능 
 const authMiddleware = basicAuth({
   users: { 'admin': 'admin' },
   challenge: true,
@@ -19,9 +19,9 @@ const authMiddleware = basicAuth({
 })
 
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs') // express가 자동으로 ejs를 불러옴
 app.use('/static', express.static('public'))
-app.use(morgan('tiny'))
+app.use(morgan('tiny')) // 현재 터미널의 서버 상황
 
 
 // 'hello world 찍기' => 
@@ -44,7 +44,20 @@ app.get('/:id', (req, res) => {
 
 
 //form 전송방법
-// app.post()
+
+// data에 있는 id와 새로 randomstring한 변수 candiate를 while문으로 반복 비교하여 같은 값이 없다면 candidate를 변수 id에 담아 while문을 끝내라! 
+app.post('/', authMiddleware, (req,res)=>{
+  const longUrl = req.body.longUrl // ejs파일의 form 태그 name이 url 인것을 longUrl 변수에 담음 
+  let id
+  while (true) {
+    const candidate = randomstring.generate(6)
+    const matched = data.find(item => item.id === candidate)
+    if (!matched) {
+      id = candidate
+      break
+    }
+  }
+})
 
 
 // 포트 연결
